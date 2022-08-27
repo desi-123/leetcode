96f06579876571131e9c3bc55b4862db6892e3cb
Desalegn Wagaw <desiemeng21m@gmail.com>
2022-08-26 17:57:10 -0700

    two_sum

---
+++ two_sum.py
@@ -0,0 +1,7 @@
+def twoSum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        if target - num in seen:
+            return ([seen[target-num], i])
+        elif num not in seen:
+            seen[num] = i
