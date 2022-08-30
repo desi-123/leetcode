d3214b4fdd526a7c0a4c7ef66824d46e18879675
Desalegn Wagaw <desiemeng21m@gmail.com>
2022-08-30 08:02:53 -0700

    rob house

---
+++ rob_house.py
@@ -0,0 +1,10 @@
+def rob(nums):
+    rob1, rob2 = 0, 0
+    for n in nums:
+        temp = max(n + rob1, rob2)
+        rob1 = rob2
+        rob2 = temp
+    return rob2
+
+if __name__ == '__main__':
+    print(rob([1, 2, 3, 1]))
